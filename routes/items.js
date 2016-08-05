7a0ac2c676ec76efda464c0802774664d1076552
jgcvi <coatsj11@gmail.com>
2016-08-04 17:54:09 -0700

    Included bower
    Including bower dependencies, still not stable

--- routes/items.js
+++ routes/items.js
@@ -1,5 +1,50 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+var Item = require('../models/item.js');
 mongoose.connect('mongodb://localhost/buttondb');
 
+router.get('/items', function(req, res, next) {
+Item.find({}, function(err, items) {
+		if(err) {
+			res.send(err);
+		} else {
+			res.json(items);
+		}
+	})
+});
+
+router.get('/todo/:id', function(req, res, next) {
+	Item.findOne({
+		name: req.params.id
+	}, function(err, items) {
+		if(err) {
+			res.send(err);
+		} else {
+			res.json(items);
+		}
+	});
+});
+
+// now starts admin calls
+router.post('/item', function(req, res, next) {
+	var item = req.body;
+	Item.save(item, function(err, result) {
+		if(err) {
+			res.send(err);
+		} else {
+			res.json(result);
+		}
+	});
+});
+
+router.put('/todo/:id', function(req, res, next) {
+	var item = req.body;
+	var updObj = {};
+
+	/*
+	* figure out data model and complete here
+	*/
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,26 +2,26 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyPaser = require('body-parser');
+var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var todos = require('./routes/items');
+var items = require('./routes/items');
 
 var app = express();
 
-app.set('views, path.join(__dirname, 'views');
+app.set('views', path.join(__dirname, ''));
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
-app.user(logger('dev'));
+app.use(logger('dev'));
 app.use(bodyParser.json());
-app.user(bodyParser.urlencoded({
+app.use(bodyParser.urlencoded({
 	extended: false
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.usse('/api/v1/', todos);
+app.use('/api/v1/', items);
 var server = app.listen(3000, function() {
 	var host = 'localhost';
 	var port = server.address().port;
